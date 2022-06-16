0b7ed830f98b4c017450c852fd9c66e5f8b2c23f
Amin Mangeot <mamindegs@hotmail.fr>
2022-06-16 14:45:44 +0200

    exo AlgoJs fini

---
+++ exercice.js
@@ -0,0 +1,88 @@
+/*
+Exercice 1
+Définir une variable qui se nomme seconde et l’initialiser à 60, afficher le contenu de la variable seconde
+*/
+
+
+
+
+
+
+
+
+/*
+Exercice 2
+Définir une variable qui se nomme minute et l’initialiser à 20.
+
+
+À la ligne suivante, modifier la variable minute en lui donnant comme nouvelle valeur 30.
+
+
+En utilisant l’instruction console.log, afficher le contenu de la variable minute.
+*/
+
+
+
+
+
+/*
+Exercice 3
+
+Définir une variable qui se nomme seconde et qui est initialisée à la valeur 60.
+
+
+À la suite, définir une autre variable minute initialisée à 30.
+
+
+En utilisant l’instruction console.log, afficher la multiplication de seconde et minute.
+
+*/
+
+
+
+
+
+
+/*
+Exercice 4
+
+Imaginons une variable qui nous permet de compter le nombre d’appels téléphoniques que l’on a reçu dans la journée.
+
+Créez une variable nombreTel et initialisez-la à 0.
+
+
+Incrémentez cette variable 3 fois de suite (avec la syntaxe ++) et affichez sa valeur dans la console.
+
+
+À la suite de votre code, décrémentez cette variable une fois (avec la syntaxe --) et affichez sa valeur dans la console.
+*/
+
+
+
+
+
+
+/*
+
+Exercice 5
+
+Imaginons une variable nommée email qui corresponde au nombre d’e-mails reçus dans la journée. Initialisez cette variable à 23.
+
+
+À la ligne en dessous, ajoutez 5 à la variable en utilisant la manière décrite ci-dessus, puis affichez le résultat dans la console.
+
+*/
+
+
+
+
+
+/*
+Exercice 6
+
+Imaginons une variable nommée scoreGame qui correspond au score d’un jeu vidéo. Initialisez cette variable à 420
+
+
+À la ligne en dessous, ajoutez 15 au score en utilisant un raccourci syntaxique, puis affichez le résultat dans la console.
+
+*/
